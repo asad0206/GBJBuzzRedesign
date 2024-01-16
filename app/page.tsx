3795eb36655eae4db85347952200bd3cc006f285
Asad <asad200ana@gmail.com>
2024-01-16 13:33:03 +0530

    checking pages setup

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Start</h1>
       <Link href="/about"> about</Link>
+      <Link href="/Contacts">Contact</Link>
     </main>
   );
 }
